dc83d6676a00d24f839d15eb0aeac4c931bb16e3
Yaman Boztepe <yaman.boztepe1@gmail.com>
2025-11-29 11:27:22 +0300

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -38,11 +38,19 @@ public class RemoteFeedLoader {
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { result in
             switch result {
-            case .success:
-                completion(.failure(.invalidData))
+            case let .success(data, _):
+                if let _ = try? JSONSerialization.jsonObject(with: data) {
+                    completion(.success([]))
+                } else {
+                    completion(.failure(.invalidData))
+                }
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
     }
+}
+
+private struct Root {
+    let items: [FeedItem]
 }
