a11cd913cc729d806f8b9cbdd35548d8c2cb0a18
Yaman Boztepe <yaman.boztepe1@gmail.com>
2025-12-13 11:28:24 +0300

    Add `EssentialFeedCacheIntegrationTests` target seperateley to prevent potential performance issues

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Yaman Boztepe on 13.12.2025.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    
+}
