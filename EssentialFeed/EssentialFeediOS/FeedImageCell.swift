f6a9f857ce33fbd416aafbcdcc4984a5879be4b0
Yaman Boztepe <yaman.boztepe@boynergrup.com>
2025-12-21 20:47:27 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Yaman Boztepe on 21.12.2025.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
