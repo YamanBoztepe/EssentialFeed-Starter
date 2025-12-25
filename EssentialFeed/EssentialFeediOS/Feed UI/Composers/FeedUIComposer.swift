d530956edf07cce2e9d36440a38c867c8a0aa9bb
Yaman Boztepe <yaman.boztepe@boynergrup.com>
2025-12-25 23:57:09 +0300

    .

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -5,6 +5,7 @@
 //  Created by Yaman Boztepe on 22.12.2025.
 //
 
+import UIKit
 import EssentialFeed
 
 public final class FeedUIComposer {
@@ -22,7 +23,8 @@ public final class FeedUIComposer {
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> (([FeedImage]) -> Void) {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: loader)
+                let viewModel = FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init)
+                return FeedImageCellController(viewModel: viewModel)
             }
         }
     }
