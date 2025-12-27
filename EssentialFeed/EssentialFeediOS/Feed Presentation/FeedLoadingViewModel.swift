59fc6693a64490fada819b515804997569d6fc7b
Yaman Boztepe <yaman.boztepe@boynergrup.com>
2025-12-27 20:14:23 +0300

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Yaman Boztepe on 27.12.2025.
+//
+
+struct FeedLoadingViewModel {
+    var isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Yaman Boztepe on 27.12.2025.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    var feed: [FeedImage]
+}
