fadd69332628c3bb7fa6fa994acf85d8776a71b2
Yaman Boztepe <yaman.boztepe@boynergrup.com>
2026-01-09 20:46:06 +0300

    Move `FeedImagePresenter` and collaborator to production

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FeedImageViewModel.swift
+//  EssentialFeed
+//
+//  Created by Yaman Boztepe on 9.01.2026.
+//
+
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        return location != nil
+    }
+}
