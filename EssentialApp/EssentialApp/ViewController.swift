76bae634628bc310896b62fa68cace4e206664c9
Yaman Boztepe <yaman.boztepe@boynergrup.com>
2026-01-16 09:23:19 +0300

    Add empty essential project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Yaman Boztepe on 16.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
